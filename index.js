71346ff8fd81e0446f2685e698e3616c5e218132
inflicted-yahaya <ybarayavuga@student.refactory.academy>
2025-12-01 16:03:41 +0300

    This is my javascript commit

---
+++ index.js
@@ -0,0 +1,45 @@
+// This is an introduction
+console.log ("Hello,", "My name is Yahaya");
+
+/* This is an example of a multiline comment used especially when writing long documents for projects I guess, This is inflictor 57. Lets print out my name down below*/
+let myName = "INFLICTOR";
+let age = 57;
+
+console.log (`Hello there my name is ${myName}, And my age is ${age}.`);
+
+/*So Inflictor 57 has a company that is called "INFLICTOR JAVASCRIPT FACTORY, we need to display that here. And remember the company name never changes even a bit"*/
+
+const companyName = "INFLICTOR JAVASCRIPT COMPANY";
+ /*You might be asking yourself what is the type of variable for our variable companyName*/
+
+console.log (typeof companyName);
+console.log (typeof age);
+
+
+/*Lets do some basic math in Javascript, to findout the age of Inflictor 5 years from now*/
+
+console.log (age+5);
+
+const isAdult = true;
+console.log (typeof isAdult);
+
+//Inflictor for some case, has no name, hhhhhhhh, and we also dont know the place where he lives because he keeps shifting like Purov. 
+let secondName = null;
+let stayLOcation = undefined;
+
+/*Research tells me that put a semi-colon after a variable name automatically makes it undefined, lets try it out, say Inflictor has no future job*/
+let futureJob;
+
+//Lets check the data types of these variables
+console.log (typeof secondName);
+console.log (typeof stayLOcation);
+console.log(futureJob);
+
+
+
+let endAssignment = "--------------- END OF DAY 1 ASSIGNMENT ---------------";
+console.log (endAssignment);
+
+
+let question = "I assigned null to the variableName secondName, but the typeof operator returned (object) what does it mean";
+console.log(question);
